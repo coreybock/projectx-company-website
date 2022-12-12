35bab109ac30eea77db04f65334822d50d220f9d
coreybock <admin@coreybock.com>
2022-12-12 16:48:04 +1100

    20221212 Update to footer

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import * as s from "./Footer.style"
 import SocialsButton from "@Components/SocialButtons/SocialButtons"
 import logoSrc from "@Assets/images/full-white-2.png";
 
-const Footer = (props) => {
+const Footer = () => {
     return (
         <s.FooterContainer>
            <s.FooterTop>
@@ -17,6 +17,8 @@ const Footer = (props) => {
                   </s.FooterTopLeft>
                    <s.FooterTopRight>
                        We wish to acknowledge the Wurundjeri (‘were-un-juri’) people of the Kulin nations, the Traditional Owners of the land on which Deakin University Melbourne campus is built. We pay our respects to the local people for allowing us to have our gathering on their land and to their Elders: past, present and future.
+                       <br/><br/>
+                       Website design and development proudly completed by Gopher Industries - Project X
                    </s.FooterTopRight>
 
                </s.FooterTopContent>
